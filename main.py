773fa76a8c003ab643f2311dabde07113a200476
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-06-25 20:49:59 +0530

    changes

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import smtplib
 from dotenv import load_dotenv
 import os
 
+# Load environmental variables
 load_dotenv()
 email_id = os.getenv("EMAIL_ID")
 password = os.getenv("PASSWORD")
 
-app = Flask(__name__)
-
+# API Endpoint for posts
 post_list = "https://api.npoint.io/f12c82394cf54590cf2c"
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -33,11 +35,14 @@ def contact():
         email = data['email']
         phone = data['phone']
         message = data['message']
-        message = f"Subject:New Message from {name}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+        mail_message = f"Subject:New Message from {name}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+
+        # Send mail
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(email_id, password)
-            connection.sendmail(from_addr=email_id, to_addrs="elayabarathiedison@gmail.com", msg=message)
+            connection.sendmail(from_addr=email_id, to_addrs="elayabarathiedison@gmail.com", msg=mail_message)
+
         return render_template('contact.html', tit="Successfully sent your message")
     return render_template('contact.html', tit="Let’s Connect!")
 
